c6a660f71c6dcfbb7f6ba1c821d7dbf18be8b212
Adhita Selvaraj <Adhita@Adhita-MacBook-Air.local>
2017-05-02 16:01:00 -0400

    Changed filepath to SentimentANlayzer class spark-submit command

--- exec_spark_jobs.sh
+++ exec_spark_jobs.sh
@@ -16,13 +16,13 @@ echo -e "***** Started Spark services...... *****\n"
 echo -e "-----------------CD into target--------------\n"
 cd $FOLDER_NAME/target/scala-2.11/
 echo -e "\n\n***** Starting Naive Bayes Model creation of training data...... *****\n\n"
-spark-submit --class "SparkNaiveBayesModelCreator" --master spark://ip-172-31-5-231.us-east-2.compute.internal:7077 tweet-sentiment-analysis_2.11-0.1.jar
+spark-submit --class "SparkNaiveBayesModelCreator" --master local[3] tweet-sentiment-analysis_2.11-0.1.jar
 echo -e "\n\n***** Naive Bayes Model creation of training data is complete...... *****\n\n"
 
 
 # Trigger Spark Streaming job for sentiment prediction.
 echo -e "\n***** Starting Twitter Sentiment Analysis ...... *****\n"
 echo -e "***** Please launch browser on the host machine to http://192.168.99.100:9999 to view Twitter Sentiment visualized on a world map ...... *****\n\n"
-cd $CODE_FOLDER/target/scala-2.11/
-spark-submit --class "TweetSentimentAnalyzer" --master spark://ip-172-31-5-231.us-east-2.compute.internal:7077 tweet-sentiment-analysis_2.11-0.1.jar
+cd $FOLDER_NAME/target/scala-2.11/
+spark-submit --class "TweetSentimentAnalyzer" --master local[3] tweet-sentiment-analysis_2.11-0.1.jar
 
