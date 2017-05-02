e1c83dc7af1882b3fa079053c9d5b5073fe214a8
Adhita Selvaraj <Adhita@Adhita-MacBook-Air.local>
2017-05-01 20:19:22 -0400

    Changed class path

--- exec_spark_jobs.sh
+++ exec_spark_jobs.sh
@@ -14,7 +14,7 @@ echo -e "***** Started Spark services...... *****\n"
 # Trigger Spark job for creating the Naive Bayes Model.
 cd $CODE_FOLDER/target/scala-2.11/
 echo -e "\n\n***** Starting Naive Bayes Model creation of training data...... *****\n\n"
-spark-submit --class "org.p7h.spark.sentiment.mllib.SparkNaiveBayesModelCreator" --master spark://ip-172-31-5-231.us-east-2.compute.internal:7077 tweet-sentiment-analysis_2.11-0.1.jar
+spark-submit --class "SparkNaiveBayesModelCreator" --master spark://ip-172-31-5-231.us-east-2.compute.internal:7077 tweet-sentiment-analysis_2.11-0.1.jar
 echo -e "\n\n***** Naive Bayes Model creation of training data is complete...... *****\n\n"
 
 
@@ -22,5 +22,5 @@ echo -e "\n\n***** Naive Bayes Model creation of training data is complete......
 echo -e "\n***** Starting Twitter Sentiment Analysis ...... *****\n"
 echo -e "***** Please launch browser on the host machine to http://192.168.99.100:9999 to view Twitter Sentiment visualized on a world map ...... *****\n\n"
 cd $CODE_FOLDER/target/scala-2.11/
-spark-submit --class "org.p7h.spark.sentiment.TweetSentimentAnalyzer" --master spark://ip-172-31-5-231.us-east-2.compute.internal:7077 tweet-sentiment-analysis_2.11-0.1.jar
+spark-submit --class "TweetSentimentAnalyzer" --master spark://ip-172-31-5-231.us-east-2.compute.internal:7077 tweet-sentiment-analysis_2.11-0.1.jar
 
